2ad68ebddb324fc001e64ce20645def69b8b08a8
Anthony Fluker <zhudkencio@gmail.com>
2015-03-18 16:07:37 -0500

    AXISANG: Agregando conversiones a otros datatypes

---
+++ axisang/axisangconvert/axisangconvert.h
@@ -0,0 +1,8 @@
+// Tranformade Axisang
+
+void kfxAxisang_2Quat(kfxAxisang_t * axis, kfxQuat_t * q);
+
+void kfxAxisang_2Rpy(kfxAxisang_t * axis, kfxRpy_t * rpy);
+
+void kfxAxisang_2Mat3(kfxAxisang_t * axis, kfxMat3_t * m);
+
