ddc734ded0bffc431fbccda3f4ad382499587ddc
Anthony Fluker <zhudkencio@gmail.com>
2015-03-18 15:20:15 -0500

    RPY: Agregando a otros datatypes

---
+++ rpy/rpyconvert/rpyconvert.h
@@ -0,0 +1,7 @@
+// TRANSFORMADE ROLL-PITCH-YAW
+
+void kfxRpy_2Mat3(kfxRpy_t * rpy, kfxMat3_t * m);
+
+void kfxRpy_2Quat(kfxRpy_t * rpy, kfxQuat_t * q);
+
+void kfxRpy_2Axisang(kfxRpy_t * rpy, kfxAxisang_t * axis);
