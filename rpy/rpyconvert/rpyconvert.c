22d03353ba2a35caf711534e32012a05633505a8
Frank Moreno <frankmoreno1993@gmail.com>
2015-03-18 15:56:02 -0500

    RPY: convertions - first commit

--- rpy/rpyconvert/rpyconvert.c
+++ rpy/rpyconvert/rpyconvert.c
@@ -1,50 +1,60 @@
+#ifndef github_com_kranfix_odometry_rpy_rpyconvert_rpyconvert_c
+#define github_com_kranfix_odometry_rpy_rpyconvert_rpyconvert_c
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+#include "rpyconvert.h"
+
 // TRANSFORMADE ROLL-PITCH-YAW
 void kfxRpy_2Mat3(kfxRpy_t * rpy, kfxMat3_t * m){
-	
-	float cy = cos(rpy->y);
-	float cp = cos(rpy->p);
-	float cr = cos(rpy->r);
-	float sy = sin(rpy->y);
-	float sp = sin(rpy->p);
-	float sr = sin(rpy->r);
+  float cy = cos(rpy->y);
+  float cp = cos(rpy->p);
+  float cr = cos(rpy->r);
+  float sy = sin(rpy->y);
+  float sp = sin(rpy->p);
+  float sr = sin(rpy->r);
 
-	////////// MATRIX //////////
-
-	m->r[0].e[0] = cy*cp;
-	m->r[0].e[1] = cy*sp*sr - sy*cr;
-	m->r[0].e[2] = cy*sp*cr + sy*sr;
-	m->r[1].e[0] = sy*cp;
-	m->r[1].e[1] = sy*sp*sr + cy*cr;
-	m->r[1].e[2] = sy*sp*cr - cy*sr;
-	m->r[2].e[0] = -sp;
-	m->r[2].e[1] = cp*sr;
-	m->r[2].e[2] = cp*cr;
-
+  ///////// MATRIX //////////
+  m->r[0].e[0] = cy*cp;
+  m->r[0].e[1] = cy*sp*sr - sy*cr;
+  m->r[0].e[2] = cy*sp*cr + sy*sr;
+  m->r[1].e[0] = sy*cp;
+  m->r[1].e[1] = sy*sp*sr + cy*cr;
+  m->r[1].e[2] = sy*sp*cr - cy*sr;
+  m->r[2].e[0] = -sp;
+  m->r[2].e[1] = cp*sr;
+  m->r[2].e[2] = cp*cr;
 }
+
 void kfxRpy_2Quat(kfxRpy_t * rpy, kfxQuat_t * q){
+  float cp = cos((rpy->p)/2);
+  float sp = sin((rpy->p)/2);
 
-	float cp = cos((rpy->p)/2);	
-	float sp = sin((rpy->p)/2);
-	
-	////////// QUATERNION //////////
-	q->w = cp*cos((rpy->y + rpy->r)/2);	
- 	q->x = -sp*sin((rpy->y - rpy->r)/2);	
-	q->y = sp*cos((rpy->y - rpy->r)/2);	
-	q->z = cp*cos((rpy->y + rpy->r)/2);	
-
+  ///////// QUATERNION //////////
+  q->w = cp*cos((rpy->y + rpy->r)/2);
+  q->x = -sp*sin((rpy->y - rpy->r)/2);
+  q->y = sp*cos((rpy->y - rpy->r)/2);
+  q->z = cp*cos((rpy->y + rpy->r)/2);
 }
+
 void kfxRpy_2Axisang(kfxRpy_t * rpy, kfxAxisang_t * axis){
+  float cp = cos((rpy->p)/2);
+  float sp = sin((rpy->p)/2);
 
-	float cp = cos((rpy->p)/2);	
-	float sp = sin((rpy->p)/2);
+  ////////// AXIS //////////    
+  axis->a = 2*acos(cp*cos((rpy->y + rpy->r)/2));
 
-	////////// AXIS //////////	
-	axis->a = 2*acos(cp*cos((rpy->y + rpy->r)/2));	
-							
-	float sa = pow(sen(axis->a/2),-1);		
-							
-	axis->x = -sa*sp*sin((rpy->y - rpy->r)/2);	
-	axis->y = sa*sp*cos((rpy->y - rpy->r)/2);	
-	axis->z = sa*cp*cos((rpy->y + rpy->r)/2);	
+  float sa = pow(sin(axis->a/2),-1);
 
+  axis->x = -sa*sp*sin((rpy->y - rpy->r)/2);
+  axis->y = sa*sp*cos((rpy->y - rpy->r)/2);
+  axis->z = sa*cp*cos((rpy->y + rpy->r)/2);
 }
+
+#ifdef __cplusplus
+};
+#endif // __cplusplus
+
+#endif // github_com_kranfix_odometry_rpy_rpyconvert_rpyconvert_c
