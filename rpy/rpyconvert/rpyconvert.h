a4e4adfedbba7e972358f85ee1697843b1647e29
Frank Moreno <frankmoreno1993@gmail.com>
2015-03-18 15:44:50 -0500

    RPY: convertion - first commit

--- rpy/rpyconvert/rpyconvert.h
+++ rpy/rpyconvert/rpyconvert.h
@@ -1,7 +1,22 @@
+#ifndef github_com_kranfix_odometry_rpy_rpyconvert_rpyconvert_h
+#define github_com_kranfix_odometry_rpy_rpyconvert_rpyconvert_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+#include "../../vector/vector.h"
+#include "../../quaternion/quaternion.h"
+#include "../../mat3/mat3.h"
+#include "../../axisang/axisang.h"
+
 // TRANSFORMADE ROLL-PITCH-YAW
-
 void kfxRpy_2Mat3(kfxRpy_t * rpy, kfxMat3_t * m);
-
 void kfxRpy_2Quat(kfxRpy_t * rpy, kfxQuat_t * q);
-
 void kfxRpy_2Axisang(kfxRpy_t * rpy, kfxAxisang_t * axis);
+
+#ifdef __cplusplus
+};
+#endif // __cplusplus
+
+#endif // github_com_kranfix_odometry_rpy_rpyconvert_rpyconvert_h
